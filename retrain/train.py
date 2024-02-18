a2ccb5796e9604f08f2668a9bdf40f914ebe3b94
ndtandl <ndtandl@gmail.com>
2024-02-18 14:59:25 +0000

    [update] run train

--- retrain/train.py
+++ retrain/train.py
@@ -10,17 +10,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from trl import setup_chat_format
 
 # Hugging Face model id
-model_id = "/Volumes/DATA 1/Llama/models/CodeLlama-7b-hf" # "codellama/CodeLlama-7b-hf" # or `mistralai/Mistral-7B-v0.1`
+model_id = "codellama/CodeLlama-7b-hf" # "codellama/CodeLlama-7b-hf" # or `mistralai/Mistral-7B-v0.1`
 
-# # BitsAndBytesConfig int-4 config
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
-# )
-
+# BitsAndBytesConfig int-4 config
 bnb_config = BitsAndBytesConfig(
-   load_in_4bit=True,
-   bnb_4bit_compute_dtype=torch.bfloat16
+    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
 )
+
+# bnb_config = BitsAndBytesConfig(
+#    load_in_4bit=True,
+#    bnb_4bit_compute_dtype=torch.bfloat16
+# )
 
 # Load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
@@ -28,7 +28,7 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     attn_implementation="flash_attention_2",
     torch_dtype=torch.bfloat16,
-    # quantization_config=bnb_config
+    quantization_config=bnb_config
 )
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.padding_side = 'right' # to prevent warnings
@@ -41,36 +41,36 @@ model, tokenizer = setup_chat_format(model, tokenizer)
 from peft import LoraConfig
 
 # LoRA config based on QLoRA paper & Sebastian Raschka experiment
-# peft_config = LoraConfig(
-#         # lora_alpha=128,
-#         # lora_dropout=0.05,
-#         # r=256,
-#         bias="none",
-#         target_modules="all-linear",
-#         task_type="CAUSAL_LM",
-# )
 peft_config = LoraConfig(
-    r=16,  # dimension of the updated matrices
-    lora_alpha=64,  # parameter for scaling
-    target_modules=[
-    "q_proj",
-    "up_proj",
-    "o_proj",
-    "k_proj",
-    "down_proj",
-    "gate_proj",
-    "v_proj"],
-    lora_dropout=0.1,  # dropout probability for layers
-    bias="none",
-    task_type="CAUSAL_LM",
+        lora_alpha=128,
+        lora_dropout=0.05,
+        r=256,
+        bias="none",
+        target_modules="all-linear",
+        task_type="CAUSAL_LM",
 )
+# peft_config = LoraConfig(
+#     r=16,  # dimension of the updated matrices
+#     lora_alpha=64,  # parameter for scaling
+#     target_modules=[
+#     "q_proj",
+#     "up_proj",
+#     "o_proj",
+#     "k_proj",
+#     "down_proj",
+#     "gate_proj",
+#     "v_proj"],
+#     lora_dropout=0.1,  # dropout probability for layers
+#     bias="none",
+#     task_type="CAUSAL_LM",
+# )
 
 
 
 from transformers import TrainingArguments
 
 args = TrainingArguments(
-    output_dir="code-llama-7b-text-to-sql", # directory to save and repository id
+    output_dir="./code-llama-7b-text-to-sql", # directory to save and repository id
     num_train_epochs=3,                     # number of training epochs
     per_device_train_batch_size=3,          # batch size per device during training
     gradient_accumulation_steps=2,          # number of steps before performing a backward/update pass
@@ -79,12 +79,12 @@ args = TrainingArguments(
     logging_steps=10,                       # log every 10 steps
     save_strategy="epoch",                  # save checkpoint every epoch
     learning_rate=2e-4,                     # learning rate, based on QLoRA paper
-    bf16=True,                              # use bfloat16 precision
-    tf32=True,                              # use tf32 precision
+    fp16=True,                              # use bfloat16 precision -- bf16=True,   
+    # tf32=True,                              # use tf32 precision
     max_grad_norm=0.3,                      # max gradient norm based on QLoRA paper
     warmup_ratio=0.03,                      # warmup ratio based on QLoRA paper
     lr_scheduler_type="constant",           # use constant learning rate scheduler
-    push_to_hub=True,                       # push model to hub
+    push_to_hub=False,                       # push model to hub
     report_to="tensorboard",                # report metrics to tensorboard
 )
 
